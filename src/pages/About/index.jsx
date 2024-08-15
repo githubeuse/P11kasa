1c3f6974fd6b27c83fc8ffa813b833b96963e7af
githubeuse <julie.souvandy.pro@gmail.com>
2024-08-15 16:32:31 +0200

    mise en place des toggles sur la page about

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -10,38 +10,40 @@ function About() {
     <div>
       <Header />
       <Banner />
-      <div>
-        <Toggle title="Fiabilité">
-          <p>
-            Les annonces postées sur Kasa garantissent une fiabilité totale. Les
-            photos sont conformes aux logements, et toutes les informations sont
-            régulièrement vérifiées par nos équipes.
-          </p>
-        </Toggle>
-        <Toggle title="Respect">
-          <p>
-            La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
-            comportement discriminatoire ou de perturbation du voisinage
-            entraînera une exclusion de notre plateforme.{" "}
-          </p>
-        </Toggle>
-        <Toggle title="Service">
-          <p>
-            La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
-            comportement discriminatoire ou de perturbation du voisinage
-            entraînera une exclusion de notre plateforme.{" "}
-          </p>
-        </Toggle>
-        <Toggle title="Sécurité">
-          <p>
-            La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
-            pour les voyageurs, chaque logement correspond aux critères de
-            sécurité établis par nos services. En laissant une note aussi bien à
-            l'hôte qu'au locataire, cela permet à nos équipes de vérifier que
-            les standards sont bien respectés. Nous organisons également des
-            ateliers sur la sécurité domestique pour nos hôtes.{" "}
-          </p>
-        </Toggle>
+      <div className="aboutContainer">
+        <div className="aboutTogglesContainer">
+          <Toggle title="Fiabilité">
+            <p>
+              Les annonces postées sur Kasa garantissent une fiabilité totale.
+              Les photos sont conformes aux logements, et toutes les
+              informations sont régulièrement vérifiées par nos équipes.
+            </p>
+          </Toggle>
+          <Toggle title="Respect">
+            <p>
+              La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+              comportement discriminatoire ou de perturbation du voisinage
+              entraînera une exclusion de notre plateforme.{" "}
+            </p>
+          </Toggle>
+          <Toggle title="Service">
+            <p>
+              La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+              comportement discriminatoire ou de perturbation du voisinage
+              entraînera une exclusion de notre plateforme.{" "}
+            </p>
+          </Toggle>
+          <Toggle title="Sécurité">
+            <p>
+              La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
+              pour les voyageurs, chaque logement correspond aux critères de
+              sécurité établis par nos services. En laissant une note aussi bien
+              à l'hôte qu'au locataire, cela permet à nos équipes de vérifier
+              que les standards sont bien respectés. Nous organisons également
+              des ateliers sur la sécurité domestique pour nos hôtes.{" "}
+            </p>
+          </Toggle>
+        </div>
       </div>
       <Footer />
     </div>
